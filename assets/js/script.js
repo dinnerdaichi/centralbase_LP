227d6363f5a8af17cd04048314ce1f75c223e484
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2025-09-26 18:23:26 +0900

    PCいろいろ修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 gsap.matchMedia().add("(min-width: 768px)", () => {
   ScrollTrigger.create({
     trigger: ".mv__flex-left-inner", // ← 内側をpin
-    start: "top 100",
+    start: "top 125",
     endTrigger: ".intro__content",
     end: "top 100",
     pin: true,
